30f17592e076d5392642cf257eca094fb0b5cab6
Embox <71951742+emboxity@users.noreply.github.com>
2021-04-20 17:17:31 -0400

    fixes

--- commands/love.js
+++ commands/love.js
@@ -9,7 +9,6 @@ module.exports = {
         const random = Math.floor(Math.random()*100)
         const love1 = message.author
         const love2 = args[0]
-        const love3 = args[1]
 
         if (!args[0]) {
             const loveerror = new Discord.MessageEmbed()
@@ -19,22 +18,12 @@ module.exports = {
             message.channel.send(loveerror);
         }
         
-        if (target) {
-            const partnerlol = new Discord.MessageEmbed()
-            .setTitle("Love meter 6000 :two_hearts_")
-            .setDescription(`${love2} ${love3}
-            ${random}%`)
-            .setColor("#FF6347")
-            .setFooter('Embox Bot * made by shiba#2254',pfp)
-            .setAuthor(message.author.username, message.author.displayAvatarURL())
-            message.channel.send(partnerlol);
 
-        }
 
 
         const tenlove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -44,7 +33,7 @@ module.exports = {
 
             const twentylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -55,7 +44,7 @@ module.exports = {
             
             const thirtylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -66,7 +55,7 @@ module.exports = {
             
             const fourtylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -76,7 +65,7 @@ module.exports = {
 
             const fiftylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :heart:  :heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -86,7 +75,7 @@ module.exports = {
 
             const sixtylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :heart:  :heart:  :heart:  :broken_heart:  :broken_heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -96,7 +85,7 @@ module.exports = {
 
             const seventylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :heart:  :heart:  :heart:  :heart:  :broken_heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -106,7 +95,7 @@ module.exports = {
 
             const eightylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :heart:  :heart:  :heart:  :heart:  :heart:  :broken_heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -116,7 +105,7 @@ module.exports = {
 
             const nintylove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :heart:  :heart:  :heart:  :heart:  :heart:  :heart:  :broken_heart: `)
             .setColor("#FF6347")
@@ -127,51 +116,51 @@ module.exports = {
             
             const fulllove = new Discord.MessageEmbed() 
             .setTitle("Love meter 6000 :two_hearts:")
-            .setDescription(`${love2} ${love3}
+            .setDescription(`${love2} ${target}
             ${random}%
             :heart: :heart: :heart: :heart:  :heart:  :heart:  :heart:  :heart:  :heart:  :heart: `)
             .setColor("#FF6347")
             .setFooter('Embox Bot * made by shiba#2254',pfp)
             .setAuthor(message.author.username, message.author.displayAvatarURL())
 
-            if(random <= 10){
+            if(random <= 20 && random >= 10){
                 message.channel.send(tenlove)
             }
     
-            if(random <= 20){
+            if(random <= 30 && random >= 20){
                 message.channel.send(twentylove)
             }
     
     
-            if(random <= 30){
+            if(random <= 40 && random >= 30){
                 message.channel.send(thirtylove)
             }
 
-            if(random <= 40){
+            if(random <= 50 && random >= 40){
                 message.channel.send(fourtylove)
             }
 
-            if(random <= 50){
+            if(random <= 60 && random >= 50){
                 message.channel.send(fiftylove)
             }
 
-            if(random <= 60){
+            if(random <= 70 && random >= 60){
                 message.channel.send(sixtylove)
             }
 
-            if(random <= 70){
+            if(random <= 80 && random >= 70){
                 message.channel.send(seventylove)
             }
 
-            if(random <= 80){
+            if(random <= 90 && random >= 80){
                 message.channel.send(eightylove)
             }
 
-            if(random <= 90){
+            if(random <= 100 && random >= 90){
                 message.channel.send(nintylove)
             }
 
-            if(random === 100){
+            if(random <= 101 && random >= 91){
                 message.channel.send(fulllove)
             }
     
