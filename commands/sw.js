5e49fca0a96223c802ff30c8b94b9a0c79d094e5
Embox <71951742+emboxity@users.noreply.github.com>
2021-04-23 21:40:20 -0400

    Update sw.js

--- commands/sw.js
+++ commands/sw.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:"simwelcome",
     aliases: ['sw'],
-    desscription: "plays music in a voice channel.",
+    desscription: "Simulates a welcome",
 
     run: (client, message, args) => {
         message.channel.send('Simulated a member joining!')
