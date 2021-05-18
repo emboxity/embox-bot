70311628203cd1be7f40c48739704dac8fe51199
Embox <71951742+emboxity@users.noreply.github.com>
2021-05-17 20:49:16 -0400

    Delete commands directory

--- commands/idc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name:"idc",
-    aliases: ["noc"],
-    desscription: "Tells someone that they don't care",
-    guildOnly: true,
-    run: (client, message, args) => {
-        message.channel.bulkDelete(1)
-        message.channel.send('nobody cares bro')
-    }}
