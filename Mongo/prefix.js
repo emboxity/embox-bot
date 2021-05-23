b9cb45d97c6d5d44e098d18248ed59b1506f2121
Embox <71951742+emboxity@users.noreply.github.com>
2021-05-22 22:36:19 -0400

    Delete Mongo directory

--- Mongo/prefix.js
+++
@@ -1,9 +0,0 @@
-const mclient = require('mongodb').mclient
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const testSchema = new Schema({
-  prefix: String, // String is shorthand for {type: String}
-  Guild: String,
-});
-module.exports = mongoose.model('testSchema', testSchema)
